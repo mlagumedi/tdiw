5ae1d3f7aaadb28af8262507b37d6d61716184d8
mlagumedi <martalagunamedina@gmail.com>
2024-09-24 19:22:23 +0200

    Little JavaScript function activity before correction class

--- js/funcions.js
+++ js/funcions.js
@@ -1,3 +1,8 @@
 function confirmaRegistre(){
     //completa
+    alert("registrant estudiant");
+    console.log("registrant estudiant");
+    var formDiv = document.getElementById("formDiv");
+    formDiv.innerHTML = "<p class='important'>T'has registrat amb èxit</p>";
+    return false;
 }
